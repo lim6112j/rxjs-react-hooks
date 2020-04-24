220094455096d49c8288876f05cf6a0b61323b0b
lim byeong cheol <lim6112j@gmail.com>
2020-04-24 18:20:56 +0900

    polished

--- src/TFactory2.js
+++ src/TFactory2.js
@@ -1,19 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import { interval } from 'rxjs'
-import { map } from 'rxjs/operators'
-// const components = {
-//   A: {id: 1, name: 'div', content: '<div> component - div </div>'},
-//   B: {id: 2, name: 'text', content: '<div> component - text </div>'},
-//   C: {id: 3, name: 'card', content: '<div> component - card </div>'},
-//   D: {id: 4, name: 'button', content: '<div> component - button </div>'}
-// }
 const components = {
   A: <div> component - div </div>,
   B: <div> component - text </div>,
   C: <div> component - card </div>,
   D: <button> component - button </button>
 }
-
 const useObservable = (observable) => {
   const [state, setState] = useState()
   useEffect(() => {
@@ -22,7 +13,8 @@ const useObservable = (observable) => {
   }, [observable])
   return state
 }
-const renderItems = (items) => items.map(item => components[item.type])
+const renderItem = (item) => components[item.type]
+const renderItems = (items) => items.map(renderItem)
 
 const TFactory2 = (props) => {
   let items = useObservable(props.templateData) || ['placeholder']
